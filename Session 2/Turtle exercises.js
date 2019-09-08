82f8bc62c81215cc43e334f062e25fc3f56d4f6c
PhamAnhTuan <blackcathp9x@gmail.com>
2019-09-08 22:36:33 +0700

    initial commit

---
+++ Session 2/Turtle exercises.js
@@ -0,0 +1,45 @@
+//turtle exercises 1
+
+lt (120);
+for(let i=0; i<4; i++)
+{
+rt(90);
+fd(70);
+rt(60);
+fd(70);
+rt(120);
+fd(70);
+rt(60);
+fd(70);
+rt(120);
+}
+rt(90);
+
+
+//turtle exercises 2
+
+rt(90)
+for(let i=0; i<3; i = i+1)
+   {
+       fd(70);
+       lt(120);
+    }
+
+for (let j=0; j<4;j++)
+{
+    fd(70);
+    lt(90);
+}
+
+for (let h=0; h<5; h++)
+{
+    fd(70);
+    lt(72);
+}
+
+for (let k=0; k<6;k++)
+{
+    fd(70);
+    lt(60);
+}
+
